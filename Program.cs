04884bec48ecb7ed7a6de0b01370e048e9a2ffc0
Jesper <jesper.richert@web.de>
2024-05-03 17:10:00 +0200

    Remove .env and Fix Command problems

--- Program.cs
+++ Program.cs
@@ -6,11 +6,6 @@ var version = "1.0.0";
 
 AnsiConsole.Markup($"[bold gray]Welcome to the[/] [bold cyan]GitSystem[/]\n");
 AnsiConsole.Markup($"[bold]- Version: {version}[/]\n\n");
-(int left, int top) = Console.GetCursorPosition();
-var option = 1;
-ConsoleKeyInfo key;
-bool isSelected = false;
-
 
 var action = AnsiConsole.Prompt(
     new SelectionPrompt<string>()
@@ -24,50 +19,45 @@ var action = AnsiConsole.Prompt(
         }));
 
 
-AnsiConsole.WriteLine($"[bold,gray]Run the {action} Section.\n\n");
-
+AnsiConsole.Markup($"[bold] Selected {action}![/]\n\n\n");
 switch (action)
 {
     case "Git Login":
     {
-        AnsiConsole.Markup($"[bold] Selected {action}![/]\n");
-        AnsiConsole.Markup($"[white]:check_mark_button: Check git and gh installation[/\n]");
+        AnsiConsole.Markup($"[white]:check_mark_button: Check git and gh installation[/]\n");
 
 
         if (await BashHelper.ExecuteCommand("which gh") != "")
-            AnsiConsole.Markup("[green] Github Cli is Installed![/]\n");
+            AnsiConsole.Markup("[gray] Github Cli is Installed![/]\n");
         else
         {
-            AnsiConsole.Markup("[red]Github Cli is not installed![/]\n");
-            AnsiConsole.Markup("[gray]Install Github Cli[/]\n");
+            AnsiConsole.Markup("[red] Github Cli is not installed![/]\n");
+            AnsiConsole.Markup("[gray] Install Github Cli[/]\n");
 
             await BashHelper.ExecuteCommand("sudo apt update && sudo apt install gh");
         }
 
         if (await BashHelper.ExecuteCommand("which git") != "")
-            AnsiConsole.Markup("[green]Git is Installed![/]\n");
+            AnsiConsole.Markup("[gray] Git is Installed![/]\n");
         else
         {
-            AnsiConsole.Markup("[red]Git is not installed![/]\n");
-            AnsiConsole.Markup("[gray]Install Git[/]");
+            AnsiConsole.Markup("[red] Git is not installed![/]\n");
+            AnsiConsole.Markup("[gray] Install Git[/]\n\n");
 
             await BashHelper.ExecuteCommand("sudo apt install git-all");
         }
 
-        if (Environment.GetEnvironmentVariable("GITHUB_AUTH_TOKEN") != "")
-        {
-            AnsiConsole.Markup("[red] Can't  Login to Github! Please set the GITHUB_AUTH_TOKEN[/]");
-            return;
-        }
 
-        await BashHelper.ExecuteCommand(
-            $"gh auth login --with-token {Environment.GetEnvironmentVariable("GITHUB_AUTH_TOKEN")}");
+        var token = AnsiConsole.Ask<string>("[bold white]Provide a Token to login: [/]");
+        AnsiConsole.Markup($"[gray]Token: {token}[/]\n");
+        var gitlogin = $"gh auth login --with-token {token}";
+        await BashHelper.ExecuteCommand(gitlogin);
     }
         break;
 
     case "Lobby Server":
     {
-        AnsiConsole.Markup($"[bold] Selected {action}![/]");
+        // Lobby Server
     }
         break;
 
