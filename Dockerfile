5409351ed16085f7938be8149c9eaefea1d7029e
Jesper <jesper.richert@web.de>
2024-05-03 15:50:02 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+﻿FROM mcr.microsoft.com/dotnet/runtime:7.0 AS base
+WORKDIR /app
+
+FROM mcr.microsoft.com/dotnet/sdk:7.0 AS build
+ARG BUILD_CONFIGURATION=Release
+WORKDIR /src
+COPY ["GitSystem/GitSystem.csproj", "GitSystem/"]
+RUN dotnet restore "GitSystem/GitSystem.csproj"
+COPY . .
+WORKDIR "/src/GitSystem"
+RUN dotnet build "GitSystem.csproj" -c $BUILD_CONFIGURATION -o /app/build
+
+FROM build AS publish
+ARG BUILD_CONFIGURATION=Release
+RUN dotnet publish "GitSystem.csproj" -c $BUILD_CONFIGURATION -o /app/publish /p:UseAppHost=false
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "GitSystem.dll"]
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Diagnostics;
+using Spectre.Console;
+
+var version = "1.0.0";
+
+AnsiConsole.Markup($"[bold gray]Welcome to the[/] [bold cyan]GitSystem[/]\n");
+AnsiConsole.Markup($"[bold]- Version: {version}[/]\n\n");
+(int left, int top) = Console.GetCursorPosition();
+var option = 1;
+ConsoleKeyInfo key;
+bool isSelected = false;
+
+
+var action = AnsiConsole.Prompt(
+    new SelectionPrompt<string>()
+        .Title("[bold white]Perform an Action to the Git Server System[/]")
+        .PageSize(10)
+        .MoreChoicesText("[grey](Move up and down to reveal more fruits)[/]")
+        .AddChoices(new[]
+        {
+            "Git Login", "Lobby Server", "SMP-Spawn Server",
+            "SMP-World Server", "SMP-Farm Server", "Proxy Server",
+        }));
+
+
+AnsiConsole.WriteLine($"Run the {action} Section.");
+
+Process cmd = new Process();
+switch (action)
+{
+    case "Git Login":
+    {
+        AnsiConsole.Markup($"[bold] Selected {action}![/]");
+    }
+        break;
+
+    case "Lobby Server":
+    {
+        AnsiConsole.Markup($"[bold] Selected {action}![/]");
+    }
+        break;
+
+
+    case "SMP-Spawn Server":
+    {
+        AnsiConsole.Markup($"[bold] Selected {action}![/]");
+    }
+        break;
+
+
+    case "SMP-World Server":
+    {
+        AnsiConsole.Markup($"[bold] Selected {action}![/]");
+    }
+        break;
+
+
+    case "SMP-Farm Server":
+    {
+        AnsiConsole.Markup($"[bold] Selected {action}![/]");
+    }
+        break;
+
+
+    case "Proxy Server":
+    {
+        AnsiConsole.Markup($"[bold] Selected {action}![/]");
+    }
+        break;
+}
