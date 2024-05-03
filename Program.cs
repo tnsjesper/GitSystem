46ba31cbc948c82680981a136514f82cf87282e0
Jesper <jesper.richert@web.de>
2024-05-03 18:13:01 +0200

    Add Helper for Check Git

--- Program.cs
+++ Program.cs
@@ -24,28 +24,8 @@ switch (action)
 {
     case "Git Login":
     {
-        AnsiConsole.Markup($"[white]:check_mark_button: Check git and gh installation[/]\n");
-
-
-        if (await BashHelper.ExecuteCommand("which gh") != "")
-            AnsiConsole.Markup("[gray] Github Cli is Installed![/]\n");
-        else
-        {
-            AnsiConsole.Markup("[red] Github Cli is not installed![/]\n");
-            AnsiConsole.Markup("[gray] Install Github Cli[/]\n");
-
-            await BashHelper.ExecuteCommand("sudo apt update && sudo apt install gh");
-        }
-
-        if (await BashHelper.ExecuteCommand("which git") != "")
-            AnsiConsole.Markup("[gray] Git is Installed![/]\n");
-        else
-        {
-            AnsiConsole.Markup("[red] Git is not installed![/]\n");
-            AnsiConsole.Markup("[gray] Install Git[/]\n\n");
-
-            await BashHelper.ExecuteCommand("sudo apt install git-all");
-        }
+        await CheckGit.CheckGitInstallation(true);
+        
 
 
         var token = AnsiConsole.Ask<string>("[bold white]Provide a Token to login: [/]");
@@ -59,6 +39,11 @@ switch (action)
     case "Lobby Server":
     {
         // Lobby Server
+        await CheckGit.CheckGitInstallation(true);
+        
+        
+        
+        
     }
         break;
 
